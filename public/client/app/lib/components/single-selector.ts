d7643d8b765abe85f14bc465b9fd6db3af12256e
Mark Florence <mflo999@gmail.com>
2018-06-30 15:00:57 -0400

    Don't emit selected if not changed

--- public/client/app/lib/components/single-selector.ts
+++ public/client/app/lib/components/single-selector.ts
@@ -155,12 +155,14 @@ export class SingleSelectorComponent implements AfterViewInit, OnDestroy {
 
   onSelect(selected: number): void {
     if ((selected != null) && (selected !== -1)) {
-      this.cached = this.filtered[selected][this.itemValuePath];
-      this.input.nativeElement.value = this.filtered[selected][this.itemLabelPath];
-      this.hideListbox = true;
-      this.change.emit(this.value);
-      if (this.listener)
-        this.listener();
+      if (this.cached !== this.filtered[selected][this.itemValuePath]) {
+        this.cached = this.filtered[selected][this.itemValuePath];
+        this.input.nativeElement.value = this.filtered[selected][this.itemLabelPath];
+        this.hideListbox = true;
+        this.change.emit(this.value);
+        if (this.listener)
+          this.listener();
+      }
     }
   }
 
